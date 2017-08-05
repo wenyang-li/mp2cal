efbbe879b0af0ada2591563ddb99cd9660e5578c
wenyang-li <wenyang_li@brown.edu>
2017-08-04 20:45:39 -0400

    bug fix

--- scripts/omni_run_mwa.py
+++ scripts/omni_run_mwa.py
@@ -180,7 +180,7 @@ def omnirun(data_wrap):
         i,j = bl
         wgts[pp][(j,i)] = wgts[pp][(i,j)] = np.logical_not(flag[bl][pp]).astype(np.int)
     print '   Run omnical'
-    m2,g2,v2 = mp2cal.run_omnical_fine(dat,info,gains0=g0)
+    m2,g2,v2 = mp2cal.wyl.run_omnical_fine(dat,info,gains0=g0)
     if opts.wgt_cal:
         for a in g2[p].keys(): g2[p][a] *= auto[a]
     xtalk = heracal.omni.compute_xtalk(m2['res'], wgts) #xtalk is time-average of residual
