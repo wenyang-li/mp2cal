24f29e0e998ce5a0ccfc33d6645d0545159465f1
wenyang-li <wenyang_li@brown.edu>
2018-11-19 14:15:59 -0500

    update flagging

--- src/qltm.py
+++ src/qltm.py
@@ -11,6 +11,7 @@ class INS(object):
         md = np.ma.masked_array(d, f)
         md = md[1:] - md[:-1]
         self.ins = np.mean(np.abs(md),axis=1)
+        self.mask = np.copy(self.ins.mask)
 
     def outliers_flagging(self, nsig = 5):
         """
@@ -21,8 +22,8 @@ class INS(object):
             frac_diff = self.ins / np.mean(self.ins, axis=0) - 1
             m = np.mean(frac_diff)
             s = np.std(frac_diff)
-            ind = np.unravel_index(abs(frac_diff-m).argmax(), self.ins.shape)
-            if abs(frac_diff[ind]-m)<nsig*s: break
+            ind = np.where(abs(frac_diff-m).data*np.logical_not(self.ins.mask) > nsig*s)
+            if ind[0].size == 0: break
             else: self.ins.mask[ind] = True
 
     def smooth_over_freq(self, fc=1):
@@ -52,12 +53,10 @@ class INS(object):
         """
         maxiter = self.ins.shape[0]
         frac_smt = self.smooth_over_freq(fc=1)
-        for niter in range(maxiter):
-            frac_co = frac_smt[:,:,1:,:]*frac_smt[:,:,:-1,:]
-            dt_slice = np.max(np.mean(frac_co,axis=(1,2)),axis=1)
-            dt_ind = np.argmax(np.abs(dt_slice))
-            if np.abs(dt_slice[dt_ind]) > nthresh: self.ins.mask[dt_ind] = True
-            else: break
+        frac_co = frac_smt[:,:,1:,:]*frac_smt[:,:,:-1,:]
+        dt_slice = np.max(np.mean(frac_co,axis=(1,2)),axis=1)
+        dt_ind = np.where(np.abs(dt_slice) > nthresh)[0]
+        self.ins.mask[dt_ind] = True
 
     def freq_flagging(self, nsig=6):
         """
@@ -70,17 +69,18 @@ class INS(object):
         df_ind = np.where(df_slice>nsig*np.std(df_slice))[0]
         self.ins.mask[:,:,df_ind,:] = True
 
-    def coherence_flagging(self, fc=0.25, nsig=5):
+    def coherence_flagging(self, fc=0.25, nsig=6):
         """
         Flag samples based on smoothed (over frequency) ins. This is to find coherences in noise spectrum
         """
-        m2 = self.smooth_over_freq(fc=fc)
         for niter in range(self.ins.size):
+            m2 = self.smooth_over_freq(fc=fc)
+            m2 = np.ma.masked_array(m2, self.ins.mask)
             sigma = np.std(m2)
             mean2 = np.mean(m2)
-            indf = np.unravel_index(abs(m2-mean2).argmax(), self.ins.shape)
-            if np.abs(m2[indf]-mean2) < nsig*sigma: break
-            self.ins.mask[indf] = True
+            ind = np.where(abs(m2-mean2).data*np.logical_not(self.ins.mask) > nsig*sigma)
+            if ind[0].size == 0: break
+            self.ins.mask[ind] = True
 
     def extend_flagging(self, f_thresh=0.5, t_thresh=0.5):
         """
